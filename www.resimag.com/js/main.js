e0379080b09cc4a1c11e7fa6f8eb8a793f88faf6
zyoncommunity <44326727+zyoncommunity@users.noreply.github.com>
2018-11-30 23:26:48 +0300

    ddssdsdsd

---
+++ www.resimag.com/js/main.js
@@ -0,0 +1,46 @@
+﻿var resimag = {
+	
+	
+	select_all: function(containerid){
+			
+		var divo="#"+containerid;
+		$(divo).select();
+			
+	},
+	
+	r_yukle:function(){
+			
+		$("#tab_b").addClass("tab_selected");
+		$("#tab_i").removeClass("tab_selected");
+		$("#tab_b_div").show();
+		$("#tab_i_div").hide();
+		
+			
+	},
+	
+	netten:function(){
+			
+		$("#tab_b").removeClass("tab_selected");
+		$("#tab_i").addClass("tab_selected");
+		$("#tab_b_div").hide();
+		$("#tab_i_div").show();
+			
+	},
+	
+	netten_yukle:function(){
+		
+		icerik=$("#netten_f").val();
+		$("#loading").html('<img src="/images/loading.gif" width="32" height="32" />');
+		
+		$('#donen_sonuc').load('/ax_netten/',{'icerik':icerik });
+
+		
+	},
+	
+	
+	
+};
+
+
+
+		
